8fcd5f5a3baf6c483948b1444e3d294fb26ae858
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-11-29 16:25:26 -0300

    Lesson #23 - Router Links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                {/* <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: "#f1356d",
-                    borderRadius: "8px",
-                }}>New Blog</a> */}
-                <a href="/create">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
+                    color: 'white',
+                    backgroundColor: '#f1356d',
+                    borderRadius: '8px'
+                }}>New Blog</Link>
             </div>
         </nav>
     );
